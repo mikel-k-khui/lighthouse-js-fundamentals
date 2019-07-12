642f50b96439e6bd75bfc5037fe743fe90fbffde
mike chui <mikel.k.khui@gmail.com>
2019-07-12 05:03:53 +0000

    Demo codes to manipulate arrays individually or with iterative loop

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+/* Add "root beer" to the end of the array */
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+/* Find the first flavour of the array */
+console.log(iceCreamFlavours[0]);
+
+/* Find the last flavour of the array */
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+/* Find total number of flavours of the array */
+console.log(iceCreamFlavours.length);
+
---
+++ packing-list.js
@@ -0,0 +1,17 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+/* Ensure the counter starts at 0 for arrays */
+
+packingList[3];
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+/* Use the while or ForEach function to iterate through */
