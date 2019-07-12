8e5bbce9acfd2a556e4d7552dd320f3bcbbedebd
mike chui <mikel.k.khui@gmail.com>
2019-07-12 03:30:59 +0000

    Function calls

---
+++ even.js
@@ -0,0 +1,19 @@
+/*
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+/* simplified version of the function all */
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
