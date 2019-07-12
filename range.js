863b3995d96cb52db0e0484616f9a1883a498031
mike chui <mikel.k.khui@gmail.com>
2019-07-12 05:39:48 +0000

    Fixed logic with range on negative steps

--- range.js
+++ range.js
@@ -1,8 +1,15 @@
-/* Define function with start and end that creates array increased by each increment per step variable */
 function range(start,end,step){
   let array = [];
-  for(let add = start; add <= end; add = add + step){
-    array.push(add);
+  if(start === null || end === null || step === null){
+    array = [];
+  }else if(start > end){
+    array = [];
+  }else if(step <= 0){
+    array = [];
+  }else{
+    for(let add = start; add <= end; add = add + step){
+      array.push(add);
+    }
   }
   return array;
 }
@@ -10,3 +17,5 @@ function range(start,end,step){
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
+console.log(range(0, 8, 2));
+console.log(range(-9, 3, 3));
